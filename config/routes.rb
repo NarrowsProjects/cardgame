5400536c265b1d78ef51b949e676648ba3948408
Guust <metzguust@gmail.com>
2025-01-05 16:12:33 +0100

    Add homepage

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  root "static_pages#home"
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
